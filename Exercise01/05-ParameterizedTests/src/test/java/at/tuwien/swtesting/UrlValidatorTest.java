2c4384f8810dff91885035c2a3f0236f659b1e30
William Hedlund <william@pop-os.localdomain>
2023-04-05 20:43:41 +0200

    1.4 1.5 will add output for  expected result

--- Exercise01/05-ParameterizedTests/src/test/java/at/tuwien/swtesting/UrlValidatorTest.java
+++ Exercise01/05-ParameterizedTests/src/test/java/at/tuwien/swtesting/UrlValidatorTest.java
@@ -3,30 +3,17 @@ package at.tuwien.swtesting;
 
 import org.apache.commons.validator.routines.UrlValidator;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.lang.reflect.Array;
 
 public class UrlValidatorTest {
     
     private UrlValidator sut = new UrlValidator();
 
-    @Test
-    public void sainty_check(){
-        assertTrue(sut.isValid("https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html#isValid-java.lang.String-"));
-        assertTrue(sut.isValid("http://192.168.0.1"));
-        assertTrue(sut.isValid("http://192.abc.0.1"));
-    }
-
 	@ParameterizedTest
-    @CsvFileSource(resources = "/testdata.tsv", delimiter = '\t')
+    @CsvFileSource(resources = "/testdata.tsv", delimiter = '\t', numLinesToSkip = 1)
 	public void valid_urls_are_valid(
         String schema, String domain, String topDomain, 
         String port, String path, String querySeparator, 
@@ -48,6 +35,8 @@ public class UrlValidatorTest {
                 url += s;
             }
         };
-        assertEquals(true, sut.isValid(url), url);
+
+        assertTrue(sut.isValid(url));
+        // assertEquals(true, sut.isValid(url), "current urls: \n" + url + "\n");
 	}
 }
